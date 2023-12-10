ce3a65c134f43788aafdd90792dbcbaff3321ab5
zhenlohuang <zhenlohuang@gmail.com>
2023-12-10 16:19:39 +0800

    Task 1.1: Numerical Derivatives

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -22,8 +22,10 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError("Need to implement for Task 1.1")
+    f_plus_epsilon = f(*vals[:arg], vals[arg] + epsilon, *vals[arg + 1 :])
+    f_minus_epsilon = f(*vals[:arg], vals[arg] - epsilon, *vals[arg + 1 :])
+
+    return (f_plus_epsilon - f_minus_epsilon) / (2 * epsilon)
 
 
 variable_count = 1
